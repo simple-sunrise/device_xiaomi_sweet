5d4a1b5ee8aef5a164cc678e9699859a52d9767f
zaidkhan0997 <kzaid0997@gmail.com>
2022-11-13 14:26:57 +0530

    sweet: Switch to SleepKernel and Azure 15 clang
    Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+# clone
+git clone -b main --depth=1 https://gitlab.com/Panchajanya1999/azure-clang.git prebuilts/clang/host/linux-x86/clang-azure
+git clone -b t13 --depth=1 https://github.com/itsshashanksp/kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
