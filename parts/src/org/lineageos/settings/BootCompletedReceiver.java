fa09a36bace46a79fa42bae7b3dca6282854f13f
Pulkit077 <pulkitagarwal2k1@gmail.com>
2022-11-25 16:25:29 +0000

    sweet: parts: drop haptic feedback level adjustment
    Revert "sweet: parts: Vibrate when changing haptic feedback level"
    
    Revert "sweet: parts: Introduce haptic feedback level adjustment"
    
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -24,7 +24,6 @@ import android.util.Log;
 
 import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.doze.DozeUtils;
-import org.lineageos.settings.haptic.HapticUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
@@ -44,7 +43,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
             Log.d(TAG, "Dirac is not present in system");
         }
         DozeUtils.checkDozeService(context);
-        HapticUtils.restoreLevel(context);
         ThermalUtils.startService(context);
     }
 }
